6395edd9254cd7fe77ac96b54f457c24edc40c4a
Abel García <agarcia@gabinete.mx>
2017-04-03 17:58:00 -0500

    conecxion socket.io

--- proyecto/app/controllers/Home.js
+++ proyecto/app/controllers/Home.js
@@ -1,5 +1,12 @@
-var HomeController = function (server) {
+var HomeController = function (server,io) {
   console.log("");
+
+  io.on('connection',function (socket) {
+    socket.emit('news', { hello: 'world desde mi servidor' });
+    socket.on('my other event', function (data) {
+      console.log(data.my);
+    });
+  });
 
   server.route('/')
     .get(function (req,res) {
--- proyecto/server.js
+++ proyecto/server.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var http = require('http');
 var swig = require('swig');
 var server = express();
 
+var server_socket = http.createServer(server).listen(9000);
+var io = require('socket.io').listen(server_socket);
+
+
 /*swig Configuration*/
+swig.setDefaults({cache:false});
 server.engine('html',swig.renderFile);
 server.set('view engine','html');
 server.set('views',__dirname+'/app/views');
 
 /*Controllers*/
-require('./app/controllers/Home')(server);
-server.listen(8000);
+require('./app/controllers/Home')(server,io);
