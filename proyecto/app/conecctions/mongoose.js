42c7dcf2d2a6d6867d7f424facc7a15017b415da
Abel García <agarcia@gabinete.mx>
2017-04-04 10:11:31 -0500

    cambios

--- proyecto/app/conecctions/mongoose.js
+++ proyecto/app/conecctions/mongoose.js
@@ -1,4 +1,4 @@
-//esta es una connection a mongoDB
+//esta es una connection a mongoDB a esta conexion quedo atras
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/chatusers');
