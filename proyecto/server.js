f960da9380d82844d5ba34bf0a5af86b258167af
abel-gl <abel.g.l.w@gmail.com>
2017-04-03 19:23:45 -0500

    server.js
    commit desde github

--- proyecto/server.js
+++ proyecto/server.js
@@ -14,13 +14,7 @@ server.set('view engine','html');
 server.set('views',__dirname+'/app/views');
 
 /*Controllers*/
-<<<<<<< HEAD
 require('./app/controllers/Home')(server,io);
-=======
-require('./app/controllers/Home')(server);
-<<<<<<< HEAD
-server.listen(9000);
->>>>>>> 9a8ae2c7715881636326b347eb53135652004b81
-=======
-server.listen(800);
->>>>>>> 55400855688cc819bda751c34c1da5a152bf8645
+
+
+
